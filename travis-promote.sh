02f2c4ca6fbb4ec1b563a2c930b4179b2c13dae7
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-04-25 14:36:15 +0200

    Publish nightly builds on Github pages
    Change-Id: I0697fe1a507631c079f775a8b393fe17339d099c
    Bug-Url: https://github.com/dartdesigner/Dart-Designer/issues/12

---
+++ travis-promote.sh
@@ -0,0 +1,56 @@
+﻿echo "Build $TRAVIS_JOB_NUMBER"
+echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+echo "Java version: $TRAVIS_JDK_VERSION"
+CURRENT_TAG=$(git name-rev --name-only --tags HEAD)
+if [ $CURRENT_TAG == 'undefined' ]
+then
+    echo "Promoting an untagged build on dartdesigner.github.io/p2/nightly"
+	cd repositories/org.obeonetwork.dsl.dart.repository/target
+	git clone https://$GITHUB_TOKEN@github.com/dartdesigner/p2.git -b gh-pages --quiet
+	if [ -d p2/nightly ]
+	then
+	    rm -r p2/nightly
+	    echo "An old version of the nightly repository has been found and removed"
+	fi
+	echo "Creating the nighly repository"
+	cp -r repository p2/
+	mv p2/repository p2/nightly
+	echo "Nightly repository created"
+	ls p2/nightly
+	cd p2
+	git config user.email "stephane.begaudeau@gmail.com"
+    git config user.name "Stéphane Bégaudeau"
+	git remote rm origin
+	git remote add origin https://dartdesigner:$GITHUB_TOKEN@github.com/dartdesigner/p2.git > /dev/null
+	git add -A
+	git commit -m "Promoting a new nightly build for https://github.com/dartdesigner/Dart-Designer/commit/$TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+	git push origin gh-pages > /dev/null
+	echo "Build promoted."
+else
+    if [ $TRAVIS_PULL_REQUEST == 'false' ]
+    then
+	    LAST_TAG=$(git describe --abbrev=0 --tags)
+        echo "Promoting the release $LAST_TAG  on dartdesigner.github.io/p2/releases/$LAST_TAG"
+		cd repositories/org.obeonetwork.dsl.dart.repository/target
+		git clone https://$GITHUB_TOKEN@github.com/dartdesigner/p2.git -b gh-pages --quiet
+		if [ -d p2/releases/$LAST_TAG ]
+		then
+			rm -r p2/releases/$LAST_TAG
+			echo "An old version of the nightly repository has been found and removed"
+		fi
+		echo "Creating the nighly repository"
+		cp -r repository p2/
+		mkdir -p p2/releases
+		mv p2/repository p2/releases/$LAST_TAG
+		echo "Releases repository created"
+		cd p2
+		git config user.email "stephane.begaudeau@gmail.com"
+		git config user.name "Stéphane Bégaudeau"
+		git remote rm origin
+		git remote add origin https://dartdesigner:$GITHUB_TOKEN@github.com/dartdesigner/p2.git > /dev/null
+		git add -A
+		git commit -m "Promoting a new nightly build for https://github.com/dartdesigner/Dart-Designer/commit/$TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+		git push origin gh-pages > /dev/null
+		echo "Build promoted."
+    fi
+fi
